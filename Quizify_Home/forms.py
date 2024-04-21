d687f9a513dc0bbaf61501361cbd00a198efe422
Nguyen Ngoc Thanh Sang <nguyensang2032003@gmail.com>
2024-04-21 15:35:10 +0700

    update sth

--- Quizify_Home/forms.py
+++ Quizify_Home/forms.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User, Group
 
 
 class SignUpForm(UserCreationForm):
+    '''class tạo form đăng kí tài khoản'''
     email = forms.EmailField(max_length=254, help_text="Required. Inform a valid email address.", required=True)
     role = forms.ChoiceField(choices=[('student', 'Student'), ('teacher', 'Teacher')], required=True)
     
@@ -13,12 +14,14 @@ class SignUpForm(UserCreationForm):
         fields = ('username', 'email', 'password1', 'password2', 'role')
         
     def clean_username(self):
+        '''hàm ghi đè username để kiểm tra username đã tồn tại hay chưa'''
         username = self.cleaned_data.get('username')
         if User.objects.filter(username=username).exists():
             raise forms.ValidationError("This username is already taken.")
         return username
     
     def clean_email(self):
+        '''hàm ghi đè email để kiểm tra xem email đã tồn tại hay chưa'''
         email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("This email is already taken.")
@@ -30,5 +33,4 @@ class SignUpForm(UserCreationForm):
         if password1 and password2 and password1!=password2:
             raise forms.ValidationError("Passwords don't match.")
         return password2
-    
         
